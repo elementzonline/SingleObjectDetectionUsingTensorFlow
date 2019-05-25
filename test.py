708547d18430beb061c02c02c21359fe1f689c7a
Dhanish Vijayan <dhanishvijayan@gmail.com>
2019-05-25 20:14:48 +0530

    Test

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ from object_detection.utils import ops as utils_ops
 
 
 # This is needed to display the images.
-# %matplotlib inline
+%matplotlib inline
 
 
 from object_detection.utils import label_map_util
@@ -104,27 +104,27 @@ def run_inference_for_single_image(image, graph):
 
 
 def main(image_path):
-    # for image_path in TEST_IMAGE_PATHS:
-    image = Image.open(image_path)
-    # the array based representation of the image will be used later in order to prepare the
-    # result image with boxes and labels on it.
-    image_np = load_image_into_numpy_array(image)
-    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-    image_np_expanded = np.expand_dims(image_np, axis=0)
-    # Actual detection.
-    output_dict = run_inference_for_single_image(image_np, detection_graph)
-    # Visualization of the results of a detection.
-    vis_util.visualize_boxes_and_labels_on_image_array(
-        image_np,
-        output_dict['detection_boxes'],
-        output_dict['detection_classes'],
-        output_dict['detection_scores'],
-        category_index,
-        instance_masks=output_dict.get('detection_masks'),
-        use_normalized_coordinates=True,
-        line_thickness=8)
-    plt.figure(figsize=IMAGE_SIZE)
-    plt.imshow(image_np)
+# for image_path in TEST_IMAGE_PATHS:
+image = Image.open(image_path)
+# the array based representation of the image will be used later in order to prepare the
+# result image with boxes and labels on it.
+image_np = load_image_into_numpy_array(image)
+# Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+image_np_expanded = np.expand_dims(image_np, axis=0)
+# Actual detection.
+output_dict = run_inference_for_single_image(image_np, detection_graph)
+# Visualization of the results of a detection.
+vis_util.visualize_boxes_and_labels_on_image_array(
+    image_np,
+    output_dict['detection_boxes'],
+    output_dict['detection_classes'],
+    output_dict['detection_scores'],
+    category_index,
+    instance_masks=output_dict.get('detection_masks'),
+    use_normalized_coordinates=True,
+    line_thickness=8)
+plt.figure(figsize=IMAGE_SIZE)
+plt.imshow(image_np)
 
 if __name__ == '__main__':
     image_path = sys.argv[1]
